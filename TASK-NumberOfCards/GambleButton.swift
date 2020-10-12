fca06b47d9867ce52fe4b42b94c1b64d44427926
Tomislav Gelesic <gelesic.tomislav@gmail.com>
2020-10-12 14:26:36 +0200

    feat: main screen layout

---
+++ TASK-NumberOfCards/GambleButton.swift
@@ -0,0 +1,35 @@
+//
+//  GambleButton.swift
+//  TASK-NumberOfCards
+//
+//  Created by Tomislav Gelesic on 12/10/2020.
+//
+
+import UIKit
+
+class GambleButton: UIView {
+
+    //MARK: Properties
+    /*
+    let image: UIImageView = {
+        let view = UIView
+        return <#value#>
+    }()
+    
+    //MARK: init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("No init - check code GAMBLEBUTTON.SWIFT")
+    }
+    
+    
+    //MARK: Private functions
+    
+    private func setupViews(){
+        
+    }*/
+
+}
---
+++ TASK-NumberOfCards/MainViewController.swift
@@ -0,0 +1,85 @@
+//
+//  ViewController.swift
+//  TASK-NumberOfCards
+//
+//  Created by Tomislav Gelesic on 12/10/2020.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    //MARK: Poperties
+    let cardImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let inputNumberTextField: UITextField = {
+        let textField = UITextField()
+        textField.keyboardType = .decimalPad
+        textField.backgroundColor = UIColor.gray
+        textField.textAlignment = .center
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    let instructionLabel: UILabel = {
+        let label = UILabel()
+        
+        label.text = "Enter a three-digit or larger number"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let gambleButton: GambleButton = {
+        let button = GambleButton()
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    
+//MARK: viewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        navigationController?.navigationBar.titleTextAttributes = [NSMutableAttributedString.Key.foregroundColor: UIColor.red]
+        self.title = "Number of decks of cards"
+        
+        cardImageView.image = UIImage(named: "Card51")
+        inputNumberTextField.layer.borderColor = UIColor.red.cgColor
+        inputNumberTextField.layer.borderWidth = 1.0
+        inputNumberTextField.backgroundColor = UIColor.lightGray
+        
+        view.addSubview(cardImageView)
+        view.addSubview(inputNumberTextField)
+        view.addSubview(instructionLabel)
+        view.addSubview(gambleButton)
+        
+        cardImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+        cardImageView.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        cardImageView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 120).isActive = true
+        cardImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        inputNumberTextField.topAnchor.constraint(equalTo: cardImageView.bottomAnchor,constant: 20 ).isActive = true
+        inputNumberTextField.widthAnchor.constraint(equalToConstant: 80).isActive = true
+        inputNumberTextField.bottomAnchor.constraint(equalTo: cardImageView.bottomAnchor, constant: 60).isActive = true
+        inputNumberTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        instructionLabel.topAnchor.constraint(equalTo: inputNumberTextField.bottomAnchor, constant: 20).isActive = true
+        instructionLabel.bottomAnchor.constraint(equalTo: inputNumberTextField.bottomAnchor, constant: 40).isActive = true
+        instructionLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        gambleButton.topAnchor.constraint(equalTo: instructionLabel.bottomAnchor, constant: 40).isActive = true
+        gambleButton.bottomAnchor.constraint(equalTo: instructionLabel.bottomAnchor, constant: 80).isActive = true
+        gambleButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        
+        
+    }
+
+
+}
+
